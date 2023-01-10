cd1c18a13a5afd3e393a83884ba0d5aa2c1be298
Ryan O'Neill <ryanoneill416@gmail.com>
2023-01-10 20:20:50 +0000

    Add Navbar using react bootstrap, add moments logo and favicon

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
+import { Button } from "react-bootstrap/Button";
 import "./App.css";
+import NavBar from "./components/NavBar";
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello World!</h1>
+      <NavBar />
     </div>
   );
 }
