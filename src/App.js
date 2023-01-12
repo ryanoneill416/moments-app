15936c421fe5d0e7ab50dbd0d79f4bca39c57f59
Ryan O'Neill <ryanoneill416@gmail.com>
2023-01-12 17:48:06 +0000

    Add client_origin and dev to deployment and connect using axios

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
 import styles from "./App.module.css";
 import NavBar from "./components/NavBar";
 import Container from "react-bootstrap/Container";
-import {Route,Switch} from 'react-router-dom';
+import { Route, Switch } from "react-router-dom";
+import './api/axiosDefaults';
+import SignUpForm from "./pages/auth/SignUpForm";
+// import SignUpForm from "./pages/auth/SignUpForm";
 
 function App() {
   return (
@@ -9,10 +12,10 @@ function App() {
       <NavBar />
       <Container className={styles.Main}>
         <Switch>
-          <Route exact path='/' render={() => <h1>Home Page</h1>}/>
-          <Route exact path='/signin' render={() => <h1>Sign In</h1>}/>
-          <Route exact path='/signup' render={() => <h1>Sign Up</h1>}/>
-          <Route render={() => <p>Page not found!</p>}/>
+          <Route exact path="/" render={() => <h1>Home page</h1>} />
+          <Route exact path="/signin" render={() => <h1>Sign in</h1>} />
+          <Route exact path="/signup" render={() =><SignUpForm />} />
+          <Route render={() => <p>Page not found!</p>} />
         </Switch>
       </Container>
     </div>
