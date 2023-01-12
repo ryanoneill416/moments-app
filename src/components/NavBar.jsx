76696d56fe01951046baf417319c5e818f9829dd
Ryan O'Neill <ryanoneill416@gmail.com>
2023-01-12 16:00:31 +0000

    Implement React Bootstrap CSS and custom CSS for the navbar and standard styling

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,25 +1,26 @@
 import React from "react";
 import { Navbar, Container, Nav } from "react-bootstrap";
 import logo from "../assets/logo.png";
+import styles from '../styles/NavBar.module.css'
 
 const NavBar = () => {
   return (
-    <Navbar expand="md" fixed="top">
+    <Navbar className={styles.NavBar} expand="md" fixed="top">
       <Container>
         <Navbar.Brand>
           <img src={logo} alt="logo" height="45" />
         </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="ml-auto text-left">
+          <Nav className="ml-auto">
             <Nav.Link>
               <i className="fas fa-home"></i>Home
             </Nav.Link>
             <Nav.Link>
-            <i className="fas fa-sign-in-alt"></i>Sign In
+              <i className="fas fa-sign-in-alt"></i>Sign In
             </Nav.Link>
             <Nav.Link>
-            <i className="fas fa-user-plus"></i>Sign Up
+              <i className="fas fa-user-plus"></i>Sign Up
             </Nav.Link>
           </Nav>
         </Navbar.Collapse>
