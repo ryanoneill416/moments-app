04c69511242843cde41a895611882a82c9768ac2
Ryan O'Neill <ryanoneill416@gmail.com>
2023-01-13 16:02:49 +0000

    implement useContext hooks to pass currentUser data throughout the app, signin functionality is working

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,30 +1,52 @@
-import React from "react";
+import React, { useContext } from "react";
 import { Navbar, Container, Nav } from "react-bootstrap";
 import logo from "../assets/logo.png";
-import styles from '../styles/NavBar.module.css';
+import styles from "../styles/NavBar.module.css";
 import { NavLink } from "react-router-dom";
+import { CurrentUserContext } from "../App";
 
 const NavBar = () => {
+  const currentUser = useContext(CurrentUserContext);
+  const loggedOutIcons = (
+    <>
+      <NavLink
+        className={styles.NavLink}
+        activeClassName={styles.Active}
+        to="/signin"
+      >
+        <i className="fas fa-sign-in-alt"></i>Sign In
+      </NavLink>
+      <NavLink
+        className={styles.NavLink}
+        activeClassName={styles.Active}
+        to="signup"
+      >
+        <i className="fas fa-user-plus"></i>Sign Up
+      </NavLink>
+    </>
+  );
+  const loggedInIcons = <>{currentUser?.username}</>
+
   return (
     <Navbar className={styles.NavBar} expand="md" fixed="top">
       <Container>
         <NavLink to="/">
-        <Navbar.Brand>
-          <img src={logo} alt="logo" height="45" />
-        </Navbar.Brand>
+          <Navbar.Brand>
+            <img src={logo} alt="logo" height="45" />
+          </Navbar.Brand>
         </NavLink>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="ml-auto">
-            <NavLink exact className={styles.NavLink} activeClassName={styles.Active} to="/">
+            <NavLink
+              exact
+              className={styles.NavLink}
+              activeClassName={styles.Active}
+              to="/"
+            >
               <i className="fas fa-home"></i>Home
             </NavLink>
-            <NavLink className={styles.NavLink} activeClassName={styles.Active} to="/signin">
-              <i className="fas fa-sign-in-alt"></i>Sign In
-            </NavLink>
-            <NavLink className={styles.NavLink} activeClassName={styles.Active} to="signup">
-              <i className="fas fa-user-plus"></i>Sign Up
-            </NavLink>
+            {currentUser ? loggedInIcons : loggedOutIcons}
           </Nav>
         </Navbar.Collapse>
       </Container>
