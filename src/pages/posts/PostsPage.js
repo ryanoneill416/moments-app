2736c82f6a212ad8b1a59b567e386b99121a5448
Ryan O'Neill <ryanoneill416@gmail.com>
2023-01-17 19:35:26 +0000

    Display list of posts successfully along with the implementation of a loading spinner whilst fetching data

--- src/pages/posts/PostsPage.js
+++ src/pages/posts/PostsPage.js
@@ -9,6 +9,10 @@ import appStyles from "../../App.module.css";
 import styles from "../../styles/PostsPage.module.css";
 import { useLocation } from "react-router-dom/cjs/react-router-dom.min";
 import { axiosReq } from "../../api/axiosDefaults";
+import Post from "./Post";
+
+import NoResults from '../../assets/no-results.png'
+import Asset from "../../components/Asset";
 
 function PostsPage({ message, filter = "" }) {
   const [posts, setPosts] = useState({ results: [] });
@@ -34,7 +38,23 @@ function PostsPage({ message, filter = "" }) {
     <Row className="h-100">
       <Col className="py-2 p-0 p-lg-2" lg={8}>
         <p>Popular profiles mobile</p>
-        <p>List of posts here</p>
+        {hasLoaded ? (
+            <>
+                {posts.results.length ? (
+                    posts.results.map(post => (
+                        <Post key={post.id} {...post} setPosts={setPosts}/>
+                    ))
+                ) : (
+                    <Container className={appStyles.Content}>
+                        <Asset src={NoResults} message={message} />
+                    </Container>
+                )}
+            </>
+        ) : (
+            <Container className={appStyles.Content}>
+                <Asset spinner />
+            </Container>
+        )}
       </Col>
       <Col md={4} className="d-none d-lg-block p-0 p-lg-2">
         <p>Popular profiles for desktop</p>
