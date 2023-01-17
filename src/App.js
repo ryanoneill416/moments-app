7c63b8d3b6c61c49914d47e74ca3db5e4fbef174
Ryan O'Neill <ryanoneill416@gmail.com>
2023-01-17 19:00:13 +0000

    Add PostsPage.js boilerplate along with home, feed and liked routes with specific post filtering attributes

--- src/App.js
+++ src/App.js
@@ -7,18 +7,43 @@ import SignUpForm from "./pages/auth/SignUpForm";
 import SignInForm from "./pages/auth/SignInForm";
 import PostCreateForm from "./pages/posts/PostCreateForm";
 import PostPage from "./pages/posts/PostPage";
+import PostsPage from "./pages/posts/PostsPage";
+import { useCurrentUser } from "./contexts/CurrentUserContext";
 
 function App() {
+  const currentUser = useCurrentUser();
+  const profile_id = currentUser?.profile_id || "";
+
   return (
     <div className={styles.App}>
       <NavBar />
       <Container className={styles.Main}>
         <Switch>
-          <Route exact path="/" render={() => <h1>Home page</h1>} />
+          <Route
+            exact
+            path="/"
+            render={() => <PostsPage message="No results found." />}
+          />
+          <Route
+            exact
+            path="/feed"
+            render={() => <PostsPage
+              message="No results found." 
+              filter={`owner__followed__owner__profiles=${profile_id}&`}
+            />}
+          />
+          <Route
+            exact
+            path="/liked"
+            render={() => <PostsPage
+              message="No results found." 
+              filter={`likes__owner__profiles=${profile_id}&ordering=-likes__created_at&`}
+            />}
+          />
           <Route exact path="/signin" render={() => <SignInForm />} />
           <Route exact path="/signup" render={() => <SignUpForm />} />
           <Route exact path="/posts/create" render={() => <PostCreateForm />} />
-          <Route exact path="/posts/:id" render={() => <PostPage />}/>
+          <Route exact path="/posts/:id" render={() => <PostPage />} />
           <Route render={() => <p>Page not found!</p>} />
         </Switch>
       </Container>
