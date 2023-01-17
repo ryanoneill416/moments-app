6bc2ddc9d4cc2d010038115c55db51c7e95190a8
Ryan O'Neill <ryanoneill416@gmail.com>
2023-01-17 20:53:40 +0000

    install infinite scroll component, create fetchMoreData util function to be used to seamlessly render data upon scrolling when pagination would previously be required

--- src/pages/posts/PostsPage.js
+++ src/pages/posts/PostsPage.js
@@ -13,6 +13,8 @@ import Post from "./Post";
 
 import NoResults from "../../assets/no-results.png";
 import Asset from "../../components/Asset";
+import InfiniteScroll from "react-infinite-scroll-component";
+import { fetchMoreData } from "../../utils/utils";
 
 function PostsPage({ message, filter = "" }) {
   const [posts, setPosts] = useState({ results: [] });
@@ -37,8 +39,8 @@ function PostsPage({ message, filter = "" }) {
       fetchPosts();
     }, 1000);
     return () => {
-        clearTimeout(timer);
-    }
+      clearTimeout(timer);
+    };
   }, [filter, query, pathname]);
 
   return (
@@ -62,9 +64,15 @@ function PostsPage({ message, filter = "" }) {
         {hasLoaded ? (
           <>
             {posts.results.length ? (
-              posts.results.map((post) => (
-                <Post key={post.id} {...post} setPosts={setPosts} />
-              ))
+              <InfiniteScroll
+                children={posts.results.map((post) => (
+                  <Post key={post.id} {...post} setPosts={setPosts} />
+                ))}
+                dataLength={posts.results.length}
+                loader={<Asset spinner />}
+                hasMore={!!posts.next}
+                next={() => fetchMoreData(posts, setPosts)}
+              />
             ) : (
               <Container className={appStyles.Content}>
                 <Asset src={NoResults} message={message} />
