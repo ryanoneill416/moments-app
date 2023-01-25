06865ceb8d934fa35a4ce59d4dc634e02f082431
Ryan O'Neill <ryanoneill416@gmail.com>
2023-01-25 21:03:08 +0000

    Successfully clean up miscellaneous errors such as token refresh errors for unauthorized users

--- src/pages/auth/SignInForm.jsx
+++ src/pages/auth/SignInForm.jsx
@@ -16,6 +16,7 @@ import appStyles from "../../App.module.css";
 import axios from "axios";
 import { useSetCurrentUser } from "../../contexts/CurrentUserContext";
 import { useRedirect } from "../../hooks/useRedirect";
+import { setTokenTimestamp } from "../../utils/utils";
 
 function SignInForm() {
   const setCurrentUser = useSetCurrentUser();
@@ -43,6 +44,7 @@ function SignInForm() {
     try {
         const {data} = await axios.post("/dj-rest-auth/login/", signInData);
         setCurrentUser(data.user);
+        setTokenTimestamp(data);
         history.goBack();
     } catch(err) {
         setErrors(err.response?.data);
